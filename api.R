5c4501f587467893eb07b3aa1cd494282bd86958
KhalimCK <khalim.conn-kowlessar@moneysupermarket.com>
2018-08-06 22:08:27 +0100

    docker commit

--- api.R
+++ api.R
@@ -12,19 +12,19 @@ library(jsonlite)
 function(type = "build", cid){
   
   ##Input parameters
-  # object <- "balance.csv" #Name of the data in the s3 bucket (dummy here)
-  # bucket <- "exiipulsedata"
-  # folder_location <- "~/Documents/Exii/AWS/WebServer/" #location of the functions to be sourced in
-  # 
-  # #Output parameters
-  # folder_out <- paste0("cid_",cid)
-  # object_out <- paste0(folder_out,"/Exii_model.json")
-  # bucket_out <- "exiipulsemodels"
-  # 
-  # 
-  # #Load functions
-  # path <- "functionDependencies.R"
-  # source(path)
+  object <- "balance.csv" #Name of the data in the s3 bucket (dummy here)
+  bucket <- "exiipulsedata"
+  folder_location <- "~/Documents/Exii/AWS/WebServer/" #location of the functions to be sourced in
+
+  #Output parameters
+  folder_out <- paste0("cid_",cid)
+  object_out <- paste0(folder_out,"/Exii_model.json")
+  bucket_out <- "exiipulsemodels"
+
+
+  #Load functions
+  path <- "functionDependencies.R"
+  source(path)
   
   ##Authenticate without specifying credentials- alternatives in https://github.com/cloudyr/aws.signature/  (Which include authenticating while running in ec2 instance)
   # aws.signature::use_credentials()
@@ -39,22 +39,21 @@ function(type = "build", cid){
   
 
   ##Read in data
-  # Exii_data <- aws.s3::s3read_using(read.csv, stringsAsFactors = FALSE, object = object, bucket = bucket,
-  #                                   opts = list(check_region = FALSE))
-  # 
-  # #Train model
-  # model <- train_Exii_model(Exii_data = Exii_data, build_type = "build")
-  # 
-  # #convert to json
-  # x <- convert_to_json(x = model)
-  # #convert to string
-  # 
-  # #create customer folder for model
-  # put_folder(folder_out, bucket = bucket_out)
-  # 
-  # #push to s3
-  # aws.s3::s3write_using(x = x, FUN = jsonlite::write_json, object = object_out, bucket = bucket_out, folder = folder_out)
-  # 
-  # return(list(status = "Complete"))
-  credentials
+  Exii_data <- aws.s3::s3read_using(read.csv, stringsAsFactors = FALSE, object = object, bucket = bucket,
+                                    opts = list(check_region = FALSE))
+
+  #Train model
+  model <- train_Exii_model(Exii_data = Exii_data, build_type = "build")
+
+  #convert to json
+  x <- convert_to_json(x = model)
+  #convert to string
+
+  #create customer folder for model
+  put_folder(folder_out, bucket = bucket_out)
+
+  #push to s3
+  aws.s3::s3write_using(x = x, FUN = jsonlite::write_json, object = object_out, bucket = bucket_out, folder = folder_out)
+
+  return(list(status = "Complete"))
 }
